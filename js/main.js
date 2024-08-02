e267a0f303409a52c3baf2ac3a603766f7a4316a
GenesisGR <nenegaro@gmail.com>
2024-08-01 20:39:28 -0600

    feat - entregable 5 08.01.24

--- js/main.js
+++ js/main.js
@@ -4,6 +4,21 @@ import { MostrarHTML } from "/js/CrearHTML.js"
 
 document.addEventListener("DOMContentLoaded", ev =>{
     MostrarHTML()
+    MonstrarDatosConfig()
 })
+
+function MonstrarDatosConfig() {
+    //truco fetch
+    let url;
+
+    fetch('url')
+    
+        .then(response => response.json())
+    
+        .then(data => console.log(data));
+    
+
+    
+}
 
 
