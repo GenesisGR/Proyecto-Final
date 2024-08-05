322d0b22c525d0bd9c83f9aa549130f93e5d9ece
GenesisGR <nenegaro@gmail.com>
2024-08-05 11:55:30 -0600

    feat - commit final clase 08.01.24

--- js/main.js
+++ js/main.js
@@ -6,19 +6,3 @@ document.addEventListener("DOMContentLoaded", ev =>{
     MostrarHTML()
     MonstrarDatosConfig()
 })
-
-function MonstrarDatosConfig() {
-    //truco fetch
-    let url = "/js/Configuraciones.json"
-
-    fetch(url)
-    
-        .then(response => response.json())
-    
-        .then(data => console.log(data));
-    
-
-    
-}
-
-
