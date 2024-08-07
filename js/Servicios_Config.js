8a55e627d5e768dfd13589b12b48714d7fdc8a08
GenesisGR <nenegaro@gmail.com>
2024-08-06 20:59:07 -0600

    feat - entregable 5 08.06.24

--- js/Servicios_Config.js
+++ js/Servicios_Config.js
@@ -21,3 +21,29 @@ export const ObtenerDatosConfiguracion = async () => {
     }
 
 };
+
+
+export const ObtenerDatosApiRick = async () => {
+
+    const url = "https://rickandmortyapi.com/api/character"
+    try {
+
+        const response = await fetch(url);
+        if (response.status == "200") {
+        const data = await response.json();
+        
+        console.table(data);
+        return data
+        }
+        
+
+       else{console.log("Error al consultar el API");} 
+
+    } catch (error) {
+
+        console.log("error en la red" + error);
+
+    }
+
+};
+
--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 
 //estoy importando la funcion de otra carpeta y en la carpeeta base debe de tener el export 
 import { MostrarHTML } from "/js/Global.js"
+import { ObtenerDatosConfiguracion, ObtenerDatosApiRick } from "/js/Servicios_Config.js"
 
 
 
 document.addEventListener("DOMContentLoaded", ev =>{
     MostrarHTML()
+    ObtenerDatosApiRick()
     // MonstrarDatosConfig()
     // ObtenerDatosConfiguracion();
 })
